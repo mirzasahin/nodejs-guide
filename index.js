2a1436469e03be16d3455f251552593887538413
Mirza Sahin <salihmirza123@gmail.com>
2024-04-15 08:19:36 +0300

    EJS and Dynamic Links

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-var http = require("http");
-var fs = require("fs");
+const express = require("express");
+const app = express();
 
-var server = http.createServer((req, res) => {
-  if (req.url == "/") {
-    fs.readFile("index.html", (err, html) => {
-      res.write(html);
-      res.end();
-    });
-  } else if (req.url == "/about") {
-    fs.readFile("about.html", (err, html) => {
-      res.write(html);
-      res.end();
-    });
-  } else {
-    fs.readFile("404.html", (err, html) => {
-      res.write(html);
-      res.end();
-    });
-  }
+app.set("view engine", "ejs");
+
+const data = [
+  { id: 1, name: "Iphone 14", price: 30000, isActive: true },
+  { id: 2, name: "Iphone 15", price: 40000, isActive: false },
+  { id: 3, name: "Iphone 16", price: 50000, isActive: true },
+];
+
+app.use("/products/:id", (req, res) => {
+  const product = data.find((product) => product.id == req.params.id);
+  res.render("product-details", product);
 });
 
-server.listen(3000, () => {
-  console.log("Node.js server at port 3000!");
+app.use("/products", (req, res) => {
+  res.render("products", {
+    products: data,
+  });
+});
+
+app.use("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
 });
