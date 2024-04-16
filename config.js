7dd2e69d1bcc01d7e87e5b9c62b16c34738d465b
Mirza Sahin <salihmirza123@gmail.com>
2024-04-16 18:30:33 +0300

    Fetch data from MySQL DB

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+  db: {
+    host: "localhost",
+    user: "root",
+    password: "Mirza123",
+    database: "nodedb",
+  },
+};
+
+module.exports = config
--- index.js
+++ index.js
@@ -1,29 +1,46 @@
 const express = require("express");
 const app = express();
 
+const db = require("./data/db");
+
 app.set("view engine", "ejs");
-app.use(express.static('public'))
-app.use(express.static('node_modules'))
+app.use(express.static("public"));
+app.use(express.static("node_modules"));
 
-const data = [
-  { id: 1, name: "Iphone 14", price: 30000, isActive: true, imageUrl: "1.jpeg" },
-  { id: 2, name: "Iphone 15", price: 40000, isActive: true, imageUrl: "2.jpeg"},
-  { id: 3, name: "Iphone 16", price: 50000, isActive: true, imageUrl: "3.jpeg" },
-];
-
-app.use("/products/:id", (req, res) => {
-  const product = data.find((product) => product.id == req.params.id);
-  res.render("product-details", product);
+app.use("/products/:id", async (req, res) => {
+    try {
+        const [product,] = await db.execute("select * from products where id=?", [req.params.id])
+        res.render("product-details", {
+            product: product[0]
+          });
+        
+    } catch (err) {
+        console.log(err);
+    }
 });
 
-app.use("/products", (req, res) => {
-  res.render("products", {
-    products: data,
-  });
+app.use("/products", async (req, res) => {
+    try {
+        const [products,] = await db.execute("select * from products where isActive=1")
+        res.render("products", {
+            products: products
+          });
+        
+    } catch (err) {
+        console.log(err);
+    }
 });
 
-app.use("/", (req, res) => {
-  res.render("index");
+app.use("/", async (req, res) => {
+    try {
+        const [products,] = await db.execute("select * from products where isHome=1 and isActive=1")
+        res.render("index", {
+            products: products
+          });
+        
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 app.listen(3000, () => {
