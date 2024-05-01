f61f45e279672d027344dc44f3e819ab382a0b8d
Mirza Sahin <salihmirza123@gmail.com>
2024-05-01 09:26:30 +0300

    Express module implementation

--- source-2/app.js
+++ source-2/app.js
@@ -1,29 +1,28 @@
-const http = require('http')
-const fs = require('fs')
-
-const hostname = '127.0.0.1'
+const path = require('path')
+const express = require('express')
+const app = express()
 const port = 3000
 
-const indexPage = fs.readFileSync('index.html')
-const aboutPage = fs.readFileSync('about.html')
-const contactPage = fs.readFileSync('contact.html')
-const notFoundPage = fs.readFileSync('404.html')
-
-const server = http.createServer( (req, res) => {
-    if(req.url === '/')
-    {
-        return res.end(indexPage)
-    } else if(req.url === '/about'){
-        return res.end(aboutPage)
-    } else if(req.url === '/contact'){
-        res.end(contactPage)
-    } else{
-        res.statusCode = 404
-        res.end(notFoundPage)
-    }
-   
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'))
 })
 
-server.listen(port, hostname, () => {
-    console.log(` Server Çalışıyor. http://${hostname}:${port}`);
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'about.html'))
+})
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'contact.html'))
+})
+
+app.get('/users/:userID/movies/:moviesID', (req, res) => {
+    res.send(
+        `   <h1> Username: ${req.params.userID} </h1>
+            <h1> Film name: ${req.params.moviesID} </h1>
+        `
+    )
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
